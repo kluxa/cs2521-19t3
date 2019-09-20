629d115c41e12d4ed7c2b70330b8fe24fe821cc0
kluxa <k.luxa@unsw.edu.au>
2019-09-20 16:52:34 +1000

    week 1 friday tute

--- wk01/fri13/list/listRefresher.c
+++ wk01/fri13/list/listRefresher.c
@@ -4,24 +4,95 @@
 
 #include "list.h"
 
+/*
 typedef struct _Node {
 	int value;
 	struct _Node *next;
 } Node;
 
 typedef Node *List; // pointer to first Node
+*/
+
+List createNode(int val);
 
 /**
  * Insert a value at the end of the list
  */
+List listAppend(List head, int val) {
+	// We didn't have time to do this in the tute
 
+	// empty list
+	if (head == NULL) {
+		return createNode(val);
+	}
 
+	// not empty list
+	List curr = head;
+	// loop until the last node
+	while (curr->next != NULL) {
+		curr = curr->next;
+	}
+	curr->next = createNode(val);
+	return head;
+}
+
+/**
+ * Creates a node with the given val.
+ * Also sets the next field of the node
+ * to NULL so we don't have to do it later
+ */
+List createNode(int val) {
+	// IMPORTANT
+	// Note that we are allocating sizeof(Node) here,
+	// not sizeof(List). This is important! List is a
+	// pointer, so if we allocated sizeof(List), we
+	// would only be allocating a pointer, which
+	// is not enough space for an entire node
+	List n = malloc(sizeof(Node));
+	if (n == NULL) {
+		fprintf(stderr, "couldn't allocate node");
+		exit(EXIT_FAILURE);
+	}
+	n->value = val;
+	n->next = NULL;
+	return n;
+}
 
 /**
  * Delete first instance of a value from
  * the list, if it exists
  */
+// inputs: head of the list, value we want to delete
+// outputs: head of the updated list
+List listDelete(List head, int val) {
+	// empty list
+	if (head == NULL) {
+		return NULL;
+	}
 
+	// val is the first value in the list
+	if (head->value == val) {
+		List newHead = head->next;
+		// newHead could be NULL, but that's fine
+		// because  we don't need to access it in
+		// this function.
+		free(head);
+		return newHead;
+	}
+
+	// val is not the first value in the list
+	List prev = NULL;
+	List curr = head;
+	while (curr != NULL && curr->value != val) {
+		prev = curr;
+		curr = curr->next;
+	}
+	if (curr != NULL) {
+		prev->next = curr->next;
+		free(curr);
+	}
+	return head;
+}
 
 int main(void) {
 	List l1 = newList();
@@ -36,20 +107,20 @@ int main(void) {
 	printList(l1);
 
 	// try a value in the list
-	l1 = deleteVal(l1, 2);
+	l1 = listDelete(l1, 2);
 	printList(l1);
 
 	// try a value not in the list
-	l1 = deleteVal(l1, 9);
+	l1 = listDelete(l1, 9);
 	printList(l1);
 
 	// try a value that occurs more
 	// than once in the list
-	l1 = deleteVal(l1, 1);
+	l1 = listDelete(l1, 1);
 	printList(l1);
 
 	// try a value that appears at
 	// the beginning of the list
-	l1 = deleteVal(l1, 0);
+	l1 = listDelete(l1, 0);
 	printList(l1);
 }
--- wk01/fri13/list/sumList.c
+++ wk01/fri13/list/sumList.c
@@ -44,15 +44,50 @@ int main(void) {
 
 // sum a list using a while loop
 int sumList1(List head) {
-	
+	// loop
+	// condition to stop at the end
+	// pointer to keep track of where we're up to
+	// integer variable to keep track of the sum
+	List curr = head;
+	int sum = 0;
+	while (curr != NULL) {
+		sum += curr->value;
+		curr = curr->next; 
+	}
+	return sum;
 }
 
 // sum a list using a for loop
 int sumList2(List head) {
-	
+	int sum = 0;
+	for (List curr = head; curr != NULL; curr = curr->next) {
+		sum += curr->value;
+	}
+	return sum;
 }
 
 // sum a list using recursion
+
+// Recursion  is  particularly  suitable for problems where the
+// data structure in question is recursive. For example:
+// - a  linked list is a recursive structure because all linked
+//   lists  (except  for the empty list) contain smaller linked
+//   lists within them
+// - a  string is recursive because all strings (except for the
+//   empty string) contain smaller strings within them.
+// - an  array  is recursive because all arrays (except for the
+//   empty  array)  contain smaller arrays within them (usually
+//   called subarrays).
+
+// Suppose  that  you  are the first node in a linked list. The
+// key  insight  is that if you knew the sum of the rest of the
+// list, then you can easily find the sum of the entire list by
+// just  adding  your value to it. But how can you find the sum
+// of the rest of the list? (Recursion!)
 int sumList3(List head) {
-	
+	if (head == NULL) {
+		return 0;
+	}
+
+	return head->value + sumList3(head->next);
 }
