31529bd354ba212e3d36e12809d1269dccc8e435
kluxa <k.luxa@unsw.edu.au>
2019-10-11 21:14:42 +1100

    week 4 friday tute

--- wk04/fri13/BSTree.c
+++ wk04/fri13/BSTree.c
@@ -10,7 +10,18 @@
  * height of the binary tree.
  */
 int printHeightDiff(BSTree t) {
-    return -1;
+    if (t == NULL) {
+        return -1;
+    } else {
+        int l = printHeightDiff(t->left);
+        int r = printHeightDiff(t->right);
+        printf("At node %d, left height - right height = %d\n", t->value, l - r);
+        return (l < r ? r : l) + 1;
+    }
+}
+
+static int max(int n1, int n2) {
+    return (n1 > n2 ? n1 : n2);
 }
 
 /**
@@ -20,7 +31,21 @@ int printHeightDiff(BSTree t) {
  * subtree and right subtree is no greater than 1.
  */
 int isHeightBalanced(BSTree t) {
-    return -1;
+    if (t == NULL) {
+        return -1;
+    } else {
+        int lh = isHeightBalanced(t->left);
+        int rh = isHeightBalanced(t->right);
+        if (lh == NOT_HEIGHT_BALANCED || rh == NOT_HEIGHT_BALANCED) {
+            return NOT_HEIGHT_BALANCED;
+        }
+
+        if (abs(lh - rh) > 1) {
+            return NOT_HEIGHT_BALANCED;
+        } else {
+            return 1 + max(lh, rh);
+        }
+    }
 }
 
 /**
@@ -31,5 +56,26 @@ int isHeightBalanced(BSTree t) {
  *   width of the subtrees
  */
 int BSTWidth(BSTree t) {
-    return -1;
+    if (t == NULL) {
+        return 0;
+    } else {
+        return 3 + BSTWidth(t->left) + BSTWidth(t->right);
+    }
+}
+
+/**
+ * Deletes  all  leaves  from the given tree and returns the root of the
+ * updated tree. (bonus function)
+ */
+BSTree deleteLeaves(BSTree t) {
+    if (t == NULL) {
+        return NULL;
+    } else if (t->left == NULL && t->right == NULL) {
+        free(t);
+        return NULL;
+    } else {
+        t->left = deleteLeaves(t->left);
+        t->right = deleteLeaves(t->right);
+        return t;
+    }
 }
